55f47740e18b407e475c1e685e00ccad65bc223c
vardaasen <git@vardaasen.no>
2026-01-16 23:26:31 +0100

    pre-commit ruff-format

--- test_library.py
+++ test_library.py
@@ -1,4 +1,22 @@
 import unittest
-from library_core import DatabaseManager, LibrarySystem
+
+from library_core import DatabaseManager
+
 
 class TestLibrarySystem(unittest.TestCase):
+    def setUp(self):
+        # Using :memory: for fast test without files
+        self.db = DatabaseManager(":memory:")
+
+    def test_database_tables_exist(self):
+        # Check if 'books' table exist
+        res = self.db.execute_query(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='books';"
+        )
+        self.assertIsNotNone(res)
+
+        # Check if 'members' table exist
+        res_members = self.db.execute_query(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name='members';"
+        )
+        self.assertIsNotNone(res_members)
